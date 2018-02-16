ad7cdde3c4c81b96ee948df8397af4bd4beaec96
Sandeep <sandeep.chourey@3scsolution.com>
2018-02-16 11:52:27 +0530

    file

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,11 +90,11 @@ DATABASES = {
       'NAME': 'djangogirls',
       'USER': 'postgres',
       'PASSWORD': 'root',
-      'HOST': 'localhost',
+      'HOST': 'heroku.com',
   }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
