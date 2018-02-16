41b5888aa4f6bf74033984b2a2de68647051f605
Sandeep <sandeep.chourey@3scsolution.com>
2018-02-16 14:53:00 +0530

    modi on chnages

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,10 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 # from dj_static import Cling
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-application = Cling(get_wsgi_application())
-application = DjangoWhiteNoise(application)
-# application = get_wsgi_application()
+# application = Cling(get_wsgi_application())
+# application = DjangoWhiteNoise(application)
+application = get_wsgi_application()
